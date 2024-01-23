77dbf64e48dfcf7a990f660fda77a1cbd130bea6
sturner11 <turn35s@gmail.com>
2024-01-22 21:01:25 -0700

    Adjust knight movement

---
+++ shared/src/main/java/chess/Pieces/KnightMoveCalculator.java
@@ -0,0 +1,77 @@
+package chess.Pieces;
+
+import chess.*;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class KnightMoveCalculator implements PieceMoveCalculator{
+    @Override
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
+        HashSet<ChessMove> moves = new HashSet<ChessMove>();
+        int ori_row = position.getRow();
+        int ori_col = position.getColumn();
+        int row = ori_row + 2;
+        int col = ori_col + 1;
+        int rowCount = 0;
+        int colCount = 0;
+        ChessMove move = null;
+        ChessPiece currentPiece = board.getBoard()[ori_row - 1][ori_col - 1];
+        // North moves
+        if (row < 8 && col < 8 && otherTeam(row, col, board, currentPiece.getTeamColor())) {
+            move = new ChessMove(position, new ChessPosition(row, col), null);
+            moves.add(move);
+        }
+        col = col - 2;
+        if (col > 0 && otherTeam(row, col, board, currentPiece.getTeamColor())){
+            move = new ChessMove(position, new ChessPosition(row, col), null);
+            moves.add(move);
+        }
+
+
+        // South Moves
+        row = row -4;
+        if (row > 0 && col > 0 && otherTeam(row, col, board, currentPiece.getTeamColor())){
+            move = new ChessMove(position, new ChessPosition(row, col), null);
+            moves.add(move);
+        }
+        col = col + 2;
+        if (col < 8 && otherTeam(row, col, board, currentPiece.getTeamColor())){
+            move = new ChessMove(position, new ChessPosition(row, col), null);
+            moves.add(move);
+        }
+
+        // East Moves
+        row++;
+        col++;
+        if (row < 8 && row > 0 && col < 8 && otherTeam(row, col, board, currentPiece.getTeamColor())){
+            move = new ChessMove(position, new ChessPosition(row, col), null);
+            moves.add(move);
+        }
+        row = row + 2;
+        if (row < 8 && otherTeam(row, col, board, currentPiece.getTeamColor())){
+            move = new ChessMove(position, new ChessPosition(row, col), null);
+            moves.add(move);
+        }
+
+        //West Moves
+        col = col - 4;
+        if (row < 8 && row > 0 && col > 0 && otherTeam(row, col, board, currentPiece.getTeamColor())){
+            move = new ChessMove(position, new ChessPosition(row, col), null);
+            moves.add(move);
+        }
+        row = row - 2;
+        if (row > 0 && otherTeam(row, col, board, currentPiece.getTeamColor())){
+            move = new ChessMove(position, new ChessPosition(row, col), null);
+            moves.add(move);
+        }
+        return moves;
+    }
+
+    private boolean otherTeam(int row, int col, ChessBoard board, ChessGame.TeamColor teamColor) {
+        if (row > 0 && row < 8 && col > 0 && col < 9) {
+            return board.getBoard()[row-1][col-1] == null || board.getBoard()[row-1][col-1].getTeamColor() != teamColor;
+        }
+        return false;
+    }
+}
