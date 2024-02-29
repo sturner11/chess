480f0d075fc23e1516e15db2343275620d0eaccb
sturner11 <turn35s@gmail.com>
2024-02-28 23:52:40 -0700

    AuthService tests

---
+++ server/src/test/java/serviceTests/AuthServiceTests.java
@@ -0,0 +1,43 @@
+package serviceTests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AuthServiceTests {
+    @Test
+    @DisplayName("Success")
+    public void registerSuccess() throws Exception {
+
+    }
+    @Test
+    @DisplayName("Fail")
+    public void registerFailure() throws Exception {
+    }
+    @Test
+    @DisplayName("Success")
+    public void loginSuccess() throws Exception {
+
+    }
+    @Test
+    @DisplayName("Fail")
+    public void loginFailure() throws Exception {
+    }
+    @Test
+    @DisplayName("Success")
+    public void logoutSuccess() throws Exception {
+
+    }
+    @Test
+    @DisplayName("Fail")
+    public void logoutFailure() throws Exception {
+    }
+    @Test
+    @DisplayName("Success")
+    public void checkAuthSuccess() throws Exception {
+
+    }
+    @Test
+    @DisplayName("Fail")
+    public void checkAuthFailure() throws Exception {
+    }
+}
