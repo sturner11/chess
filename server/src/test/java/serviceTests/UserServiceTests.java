369c28a480e70388592fa9e12f6911a46daa2a1c
sturner11 <turn35s@gmail.com>
2024-02-28 23:51:26 -0700

    UserServiceTests

--- server/src/test/java/serviceTests/UserServiceTests.java
+++ server/src/test/java/serviceTests/UserServiceTests.java
@@ -1,4 +1,47 @@
 package serviceTests;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class UserServiceTests {
-}
+
+    @Test
+    @DisplayName("Success")
+    public void registerSuccess() throws Exception {
+
+    }
+    @Test
+    @DisplayName("Fail")
+    public void registerFailure() throws Exception {
+    }
+    @Test
+    @DisplayName("Success")
+    public void loginSuccess() throws Exception {
+
+    }
+    @Test
+    @DisplayName("Fail")
+    public void loginFailure() throws Exception {
+    }
+    @Test
+    @DisplayName("Success")
+    public void logoutSuccess() throws Exception {
+
+    }
+    @Test
+    @DisplayName("Fail")
+    public void logoutFailure() throws Exception {
+    }
+    @Test
+    @DisplayName("Success")
+    public void checkAuthSuccess() throws Exception {
+
+    }
+    @Test
+    @DisplayName("Fail")
+    public void checkAuthFailure() throws Exception {
+    }
+
+    }
+
+
