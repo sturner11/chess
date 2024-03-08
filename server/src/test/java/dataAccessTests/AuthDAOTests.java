536921dee54756f6ba5dbc2a03b58fa16964215b
sturner11 <turn35s@gmail.com>
2024-03-07 23:02:59 -0700

    testing Structure

---
+++ server/src/test/java/dataAccessTests/AuthDAOTests.java
@@ -0,0 +1,43 @@
+package dataAccessTests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AuthDAOTests {
+//    @Test
+//    @DisplayName("Success1")
+//    public void registerSuccess() throws Exception {
+//
+//    }
+//    @Test
+//    @DisplayName("Fail1")
+//    public void registerFailure() throws Exception {
+//    }
+//    @Test
+//    @DisplayName("Success2")
+//    public void loginSuccess() throws Exception {
+//
+//    }
+//    @Test
+//    @DisplayName("Fail2")
+//    public void loginFailure() throws Exception {
+//    }
+//    @Test
+//    @DisplayName("Success3")
+//    public void logoutSuccess() throws Exception {
+//
+//    }
+//    @Test
+//    @DisplayName("Fail3")
+//    public void logoutFailure() throws Exception {
+//    }
+//    @Test
+//    @DisplayName("Success4")
+//    public void checkAuthSuccess() throws Exception {
+//
+//    }
+//    @Test
+//    @DisplayName("Fail4")
+//    public void checkAuthFailure() throws Exception {
+//    }
+}
---
+++ server/src/test/java/dataAccessTests/UserDAOTests.java
@@ -0,0 +1,47 @@
+package dataAccessTests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class UserDAOTests {
+//
+//    @Test
+//    @DisplayName("Success1")
+//    public void registerSuccess() throws Exception {
+//
+//    }
+//    @Test
+//    @DisplayName("Fail1")
+//    public void registerFailure() throws Exception {
+//    }
+//    @Test
+//    @DisplayName("Success2")
+//    public void loginSuccess() throws Exception {
+//
+//    }
+//    @Test
+//    @DisplayName("Fail2")
+//    public void loginFailure() throws Exception {
+//    }
+//    @Test
+//    @DisplayName("Success3")
+//    public void logoutSuccess() throws Exception {
+//
+//    }
+//    @Test
+//    @DisplayName("Fai3l")
+//    public void logoutFailure() throws Exception {
+//    }
+//    @Test
+//    @DisplayName("Success4")
+//    public void checkAuthSuccess() throws Exception {
+//
+//    }
+//    @Test
+//    @DisplayName("Fail4")
+//    public void checkAuthFailure() throws Exception {
+//    }
+
+}
+
+
