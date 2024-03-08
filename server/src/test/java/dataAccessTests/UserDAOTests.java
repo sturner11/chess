df6d366a1a7f923414a8e7965246d417bfef156f
sturner11 <turn35s@gmail.com>
2024-03-07 23:38:22 -0700

    comment out undone tests

--- server/src/test/java/dataAccessTests/UserDAOTests.java
+++ server/src/test/java/dataAccessTests/UserDAOTests.java
@@ -6,45 +6,45 @@ import org.junit.jupiter.api.Test;
 
 public class UserDAOTests {
 
-    @BeforeEach
-
-
-    @Test
-    @DisplayName("ClearSuccess")
-    public void clearSuccess() throws Exception {
-
-    }
-    @Test
-    @DisplayName("Fail1")
-    public void registerFailure() throws Exception {
-    }
-    @Test
-    @DisplayName("Success2")
-    public void loginSuccess() throws Exception {
-
-    }
-    @Test
-    @DisplayName("Fail2")
-    public void loginFailure() throws Exception {
-    }
-    @Test
-    @DisplayName("Success3")
-    public void logoutSuccess() throws Exception {
-
-    }
-    @Test
-    @DisplayName("Fai3l")
-    public void logoutFailure() throws Exception {
-    }
-    @Test
-    @DisplayName("Success4")
-    public void checkAuthSuccess() throws Exception {
-
-    }
-    @Test
-    @DisplayName("Fail4")
-    public void checkAuthFailure() throws Exception {
-    }
+//    @BeforeEach
+//
+//
+//    @Test
+//    @DisplayName("ClearSuccess")
+//    public void clearSuccess() throws Exception {
+//
+//    }
+//    @Test
+//    @DisplayName("Fail1")
+//    public void registerFailure() throws Exception {
+//    }
+//    @Test
+//    @DisplayName("Success2")
+//    public void loginSuccess() throws Exception {
+//
+//    }
+//    @Test
+//    @DisplayName("Fail2")
+//    public void loginFailure() throws Exception {
+//    }
+//    @Test
+//    @DisplayName("Success3")
+//    public void logoutSuccess() throws Exception {
+//
+//    }
+//    @Test
+//    @DisplayName("Fai3l")
+//    public void logoutFailure() throws Exception {
+//    }
+//    @Test
+//    @DisplayName("Success4")
+//    public void checkAuthSuccess() throws Exception {
+//
+//    }
+//    @Test
+//    @DisplayName("Fail4")
+//    public void checkAuthFailure() throws Exception {
+//    }
 
 }
 
