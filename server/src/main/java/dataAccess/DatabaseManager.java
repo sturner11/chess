943ae7f0f7da1c8b73f70716e0c2db5e88b19f05
sturner11 <turn35s@gmail.com>
2024-03-07 19:38:33 -0700

    DBManager

---
+++ server/src/main/java/dataAccess/DatabaseManager.java
@@ -0,0 +1,98 @@
+package dataAccess;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.sql.*;
+import java.util.Properties;
+
+public class DatabaseManager {
+    private static final String databaseName;
+    private static final String user;
+    private static final String password;
+    private static final String connectionUrl;
+
+    /*
+     * Load the database information for the db.properties file.
+     */
+    static {
+        try {
+            try (var propStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("db.properties")) {
+                if (propStream == null) throw new Exception("Unable to load db.properties");
+                Properties props = new Properties();
+                props.load(propStream);
+                databaseName = props.getProperty("db.name");
+                user = props.getProperty("db.user");
+                password = props.getProperty("db.password");
+
+                var host = props.getProperty("db.host");
+                var port = Integer.parseInt(props.getProperty("db.port"));
+                connectionUrl = String.format("jdbc:mysql://%s:%d", host, port);
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException("unable to process db.properties. " + ex.getMessage());
+        }
+    }
+
+    /**
+     * Creates the database if it does not already exist.
+     */
+    static void createDatabase() throws DataAccessException {
+        try {
+            var statement = "CREATE DATABASE IF NOT EXISTS " + databaseName;
+            var conn = DriverManager.getConnection(connectionUrl, user, password);
+            try (var preparedStatement = conn.prepareStatement(statement)) {
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage(), 500);
+        }
+    }
+
+    /**
+     * Create a connection to the database and sets the catalog based upon the
+     * properties specified in db.properties. Connections to the database should
+     * be short-lived, and you must close the connection when you are done with it.
+     * The easiest way to do that is with a try-with-resource block.
+     * <br/>
+     * <code>
+     * try (var conn = DbInfo.getConnection(databaseName)) {
+     * // execute SQL statements.
+     * }
+     * </code>
+     */
+    static Connection getConnection() throws DataAccessException {
+        try {
+            var conn = DriverManager.getConnection(connectionUrl, user, password);
+            conn.setCatalog(databaseName);
+            return conn;
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage(), 500);
+        }
+    }
+
+    private static final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  users (
+              `username` varchar(256) NOT NULL,
+              `password` varchar(256) NOT NULL,
+              `email` varchar(256) NOT NULL,
+              PRIMARY KEY (`username`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+    static void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("Unable to configure database", 500);
+        }
+    }
+
+
+}
