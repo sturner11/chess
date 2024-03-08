9b61296b3a24fe2d54364d5b97b21085eb5840d0
sturner11 <turn35s@gmail.com>
2024-03-07 23:37:55 -0700

    GameDAO TESTS

--- server/src/test/java/dataAccessTests/GameDAOTests.java
+++ server/src/test/java/dataAccessTests/GameDAOTests.java
@@ -1,73 +1,72 @@
 package dataAccessTests;
 import dataAccess.DataAccessException;
+import dataAccess.GameDAO;
 import models.Game;
 import org.junit.jupiter.api.*;
 import services.GameService;
 
 import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 public class GameDAOTests {
-//    private static GameService gameService;
-//    private static Game game;
-//
-//    @BeforeAll
-//    public static void init() throws DataAccessException {
-//        gameService = new GameService();
-//    }
-//    @BeforeEach
-//    public void gameSetup() throws DataAccessException {
-//        game = gameService.createGame("authToken", "gameName");
-//    }
-//
-//    @Test
-//    @DisplayName("Success")
-//    public void createGameSuccess() throws Exception {
-//        Game test = new Game("gameName", game.gameID());
-//        Assertions.assertEquals(game, test);
-//    }
-//
-//    @Test
-//    @DisplayName("Game name already exists")
-//    public void createGameFail() throws Exception {
-//         Assertions.assertThrows(DataAccessException.class, () -> gameService.createGame("authToken", null));
-//    }
-//
-//    @Test
-//    @DisplayName("Success")
-//    public void joinGameSuccess() throws Exception {
-//        Game game1 = gameService.joinGame("authToken", "WHITE", game.gameID(), "username");
-//        Game test = new Game("gameName", game1.gameID());
-//        test.setWhiteUsername("username");
-//        Assertions.assertEquals(test, game1);
-//    }
-//
-//    @Test
-//    @DisplayName("COLOR DOESN'T EXIT")
-//    public void joinGameFail() throws Exception {
-//        Game game1 = gameService.joinGame("authToken", "GREEN", game.gameID(), "username");
-//        Game test = new Game("gameName", game1.gameID());
-//        test.setWhiteUsername(null);
-//        Assertions.assertEquals(test, game1);
-//    }
-//
-//    @Test
-//    @DisplayName("Success")
-//    public void listGameSuccess() throws Exception {
-//        ArrayList<Game> games = gameService.listGames();
-//        ArrayList<Game> test = new ArrayList<>();
-//        test.add(new Game("gameName", game.gameID()));
-//        Assertions.assertEquals(test, games);
-//    }
-//
-//    @Test
-//    @DisplayName("Empty")
-//    public void listGameEmpty() throws Exception {
-//        gameService.clear();
-//        ArrayList<Game> games = gameService.listGames();
-//        ArrayList<Game> test = new ArrayList<>();
-//        Assertions.assertEquals(games, test);
-//    }
+    private static GameDAO gameDAO;
+    private static Game game;
+
+    @BeforeEach
+    public void init() throws DataAccessException {
+        gameDAO = new GameDAO();
+        gameDAO.clear();
+    }
+    @Test
+    @DisplayName("createSuccess")
+    public void createGame() throws DataAccessException {
+        Assertions.assertDoesNotThrow(() -> gameDAO.createGame("abcd", "gameName"));
+    }
+
+    @Test
+    @DisplayName("createFail")
+    public void createGameSuccess2() throws DataAccessException {
+        Assertions.assertDoesNotThrow(() -> gameDAO.createGame("abcd", "gameName"));
+        Assertions.assertDoesNotThrow(() -> gameDAO.createGame("abcd", "gameName"));
+    }
+
+
+    @Test
+    @DisplayName("Success")
+    public void joinGameSuccess() throws Exception {
+        Game game = gameDAO.createGame("abcd", "gameName");
+        gameDAO.joinGame("abcd", "WHITE", game.gameID(), "useraname");
+    }
+
+    @Test
+    @DisplayName("Already there")
+    public void joinGameFail() throws Exception {
+        Game game = gameDAO.createGame("abcd", "gameName");
+        gameDAO.joinGame("abcd", "WHITE", game.gameID(), "useraname");
+        assertThrows(DataAccessException.class, () -> gameDAO.joinGame("abcd", "WHITE", game.gameID(), "useraname") );
+    }
+
+    @Test
+    @DisplayName("Success")
+    public void listGameSuccess() throws Exception {
+        gameDAO.createGame("abcd", "gameName");
+        ArrayList<Game> games = gameDAO.listGames();
+        ArrayList<Game> test = new ArrayList<>();
+        test.add(new Game("gameName", 1));
+        Assertions.assertEquals(test, games);
+    }
+
+    @Test
+    @DisplayName("Empty")
+    public void listGameEmpty() throws Exception {
+        gameDAO.clear();
+        ArrayList<Game> games = gameDAO.listGames();
+        ArrayList<Game> test = new ArrayList<>();
+        Assertions.assertEquals(games, test);
+    }
+
 
 
 }
