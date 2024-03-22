1356f41614c0c7bec147910d74a9cb19ee3886ce
sturner11 <turn35s@gmail.com>
2024-03-21 22:40:57 -0600

    register, login, and create game calls

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,20 +1,25 @@
 import chess.*;
+import dataAccess.DataAccessException;
 
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.*;
+
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        String url = "http://localhost:8080/";
         System.out.println("♕ 240 Chess Client: Please Enter a command or type help to get started");
         Scanner scanner = new Scanner(System.in);
         String line = null;
         String command = null;
-        Boolean loggedIn = false;
+        boolean loggedIn = false;
+        String auth = null;
         while (!Objects.equals(command, "quit")) {
             line = scanner.nextLine();
             var userArgs = line.split(" ");
-            command = userArgs[0];
+            Map<String, String> body = new HashMap<>();
+            String[] curlArgs = new String[]{};
+
             if (!loggedIn) {
                 switch (userArgs[0]) {
                     case "help":
@@ -27,10 +32,30 @@ public class Main {
                         System.out.print(help);
                         break;
                     case "register":
-                        // register endpoint
+                        body.put("username", userArgs[1]);
+                        body.put("password", userArgs[2]);
+                        body.put("email", userArgs[3]);
+                        curlArgs = new String[]{"POST", null, url + "user", body.toString()};
+                        try {
+                            Map resp = ClientCurl.makeReq(curlArgs);
+                            assert resp != null;
+                            auth = (String) resp.get("authToken");
+                        } catch (DataAccessException ignored) {
+                            // CHeck for errors?
+                        }
                         break;
                     case "login":
-                        // call login endpoint
+                        body.put("username", userArgs[1]);
+                        body.put("password", userArgs[2]);
+                        curlArgs = new String[]{"POST", null, url + "session", body.toString()};
+                        try {
+                            Map resp = ClientCurl.makeReq(curlArgs);
+                            assert resp != null;
+                            auth = (String) resp.get("authToken");
+                            loggedIn = true;
+                        } catch (DataAccessException ignored) {
+                            // CHeck for errors?
+                        }
                         loggedIn = true;
                         break;
                     case "quit":
@@ -53,19 +78,28 @@ public class Main {
                         System.out.print(help);
                         break;
                     case "create":
-                        // call create game endpoint
+                        body.put("gameName", userArgs[1]);
+                        curlArgs = new String[]{"POST", auth, url + "game", body.toString()};
+                        ClientCurl.makeReq(curlArgs);
                         break;
                     case "list":
-                        //list game endpoint
+                        curlArgs = new String[]{"GET", url + "game"};
+                        ClientCurl.makeReq(curlArgs);
                         break;
                     case "join":
-                        // join endpoint
+                        body.put("gameID", userArgs[1]);
+                        body.put("playerColor", userArgs[2]);
+                        curlArgs = new String[]{"PUT", url + "user", body.toString()};
+                        ClientCurl.makeReq(curlArgs);
                         break;
                     case "observe":
-                        // observe a game
+                        body.put("gameID", userArgs[1]);
+                        curlArgs = new String[]{"PUT", url + "user", body.toString()};
+                        ClientCurl.makeReq(curlArgs);
                         break;
                     case "logout":
-                        //logout ep
+                        curlArgs = new String[]{"POST", url + "user"};
+                        ClientCurl.makeReq(curlArgs);
                         break;
                     case "quit":
                         break;
