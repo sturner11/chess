624eef0b78449fa505f9e1309a16f7037ec5e610
sturner11 <turn35s@gmail.com>
2024-03-21 22:41:19 -0600

    Main fixes

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -9,16 +9,10 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws DataAccessException, SQLException {
-//        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-//        System.out.println("♕ 240 Chess Server: " + piece);
-//        Server server = new Server();
-//        server.run(8080);
-        GameDAO gameDAO = new GameDAO();
-        gameDAO.createGame("asdf", "gameName");
-        gameDAO.joinGame("asdf", "WHITE", 1, "username");
-////        userDAO.createUser("username1", "password", "email");
-//        System.out.println(userDAO.userExists("username"));
-//        System.out.println(userDAO.authenticate("username", "password"));
+        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        System.out.println("♕ 240 Chess Server: " + piece);
+        Server server = new Server();
+        server.run(8080);
 
 
 
